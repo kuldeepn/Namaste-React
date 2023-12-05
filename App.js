9c4ef627125de3eee7c02496ac262a269c3cbbb4
Kuldeep Nandeshwar <30322243+kuldeepn@users.noreply.github.com>
2023-12-05 08:02:16 +0000

    Update 6 files

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-/*
-<div id='parent'>
-<div id='child'>
-<h1>hello i'm h1 tag</h1>
-</div>
-</div>
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-*/
-
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }),
-  React.createElement("h1", {}, "I'm h1 tag")
+const App = () => (
+  <div className="id">
+    {console.log(number)}
+    <h2> {number + 3000}</h2>
+    <h2 className="heading">Hello I'm Function Component</h2>
+  </div>
 );
 
-console.log(heading);
+let number = 1000;
+
+const title = (
+  <div>
+    <h1>Hello Namaste React</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<App />);
