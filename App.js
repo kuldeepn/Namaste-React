d241c0a24a0c4ba07ed63980a2c54830e6103e04
Kuldeep Nandeshwar <30322243+kuldeepn@users.noreply.github.com>
2023-12-12 10:13:33 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
+import "./index.css";
 
 /*
 Header
@@ -15,10 +16,54 @@ Footer
 --Contact 
 */
 
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="https://icons.iconarchive.com/icons/sonya/swarm/256/Fast-Food-icon.png" />
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <img src="https://png.pngtree.com/png-vector/20230412/ourmid/pngtree-food-cart-line-icon-vector-png-image_6701298.png" />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestoCard = () => {
+  return (
+    <div className="card">
+      <img src="https://c.ndtvimg.com/2022-04/fq5cs53_biryani-doubletree-by-hilton_625x300_12_April_22.jpg" />
+      <h2>Mughal Mehal</h2>
+      <h3>Biryani, Hyderabad</h3>
+      <h4>ETA 38 min</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div>
-      <h1>Helloh</h1>
+      <Header />
+      <Body />
     </div>
   );
 };
