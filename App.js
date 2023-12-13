8921eb948bbe0dfb97a5d05e24411a9a15a2e597
Kuldeep <nandeshwar.kuldip@gmail.com>
2023-12-13 19:13:25 +0530

    final

--- App.js
+++ App.js
@@ -2,16 +2,8 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 
     
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }),
-  React.createElement("h1", {}, "I'm h1 tag hello kuldeep")
-);
+const jsxHeading=<h1>This is h1 tag using JSX</h1>
 
+const root=ReactDOM.createRoot(document.getElementById('root'));
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-
-root.render(heading);
+root.render(jsxHeading);
